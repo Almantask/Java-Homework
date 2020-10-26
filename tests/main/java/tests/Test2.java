738eae81e9097f1d30aa7e110300ad651bb93cda
Almantas Karpavičius <almantusk@gmail.com>
2020-10-26 20:16:35 +0200

    Test

---
+++ tests/main/java/tests/Test2.java
@@ -0,0 +1,12 @@
+package main.java.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class Test2 {
+    @Test
+    void test2() {
+        assertThat(true).isEqualTo(true);
+    }
+
+}
