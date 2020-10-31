7434f8b1bea1bc6503f09eda2671b10ec880d663
domiiii11 <62842789+domiiii11@users.noreply.github.com>
2020-10-31 11:07:48 +0200

    Update Demo.java
    Added second print.

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,5 +3,6 @@ package main.java;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!2");
     }
 }
