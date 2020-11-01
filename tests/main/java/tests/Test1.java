dc9c2f4703642cbc7b6977736991885738588184
rimux9 <rmatuliokas@gmail.com>
2020-11-01 17:38:37 +0200

    Testing1

--- tests/main/java/tests/Test1.java
+++ tests/main/java/tests/Test1.java
@@ -3,10 +3,41 @@ package main.java.tests;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 class Test1 {
-    @Test
-    void test1() {
-        assertThat(true).isEqualTo(true);
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int result;
+        char smb;
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter first num  ");
+        num1 = reader.nextInt();
+
+        System.out.print("Enter smb ");
+        smb = reader.next().charAt(0);
+
+        System.out.print("Enter second num  ");
+        num2 = reader.nextInt();
+
+        switch(smb) {
+            case '+'   : result = num1 + num2;
+                break;
+            case '-'   : result = num1 - num2;
+                break;
+            case '*'   : result = num1 * num2;
+                break;
+            case '/'   : result = num1 / num2;
+                break;
+            default:
+                return;
+
+        }
+        System.out.println("\nResult is :\n" + num1 + " " + smb + " " + num2 + " = " + result);
+
     }
 
 }
+
