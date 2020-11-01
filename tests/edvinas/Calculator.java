33e8f25b0812d8370c438061e9a4b7f130cb360e
Edvinas <edvinasalimas98@gmail.com>
2020-11-01 18:45:12 +0200

    Calculator

--- tests/edvinas/Calculator.java
+++ tests/edvinas/Calculator.java
@@ -3,49 +3,41 @@ package edvinas;
 class Calculator{
     public static void main(String[] args) {
 
-        System.out.println(addition("5!"));
+        System.out.println(calculator("5+5") + factorial(5));
+
 //        System.out.println(factorial(5));
 
     }
 
-    public static int addition(String input) {
-        if (empty(input)) {
+    public static int calculator(String input) {
+        if (input.isEmpty() || input.contains(",")) {
             return 0;
-        }
+        } else {
+            String operators[] = input.split("[0-9]");
+            printArray(operators);
+            String operands[] = input.split("[*+-/!]");
+            printArray(operands);
+            int action = Integer.parseInt(operands[0]);
+            int factorial = Integer.parseInt(operands[0]);
+            int o = 1;
+            for (int i = 0; i < operators.length; i++) {
+                if (operators[i].equals("+")) {
+                    action += Integer.parseInt(operands[o++]);
+                } else if (operators[i].equals("-")) {
+                    action -= Integer.parseInt(operands[o++]);
+                } else if (operators[i].equals("*")) {
+                    action *= Integer.parseInt(operands[o++]);
+                } else if (operators[i].equals("/")) {
+                    action /= Integer.parseInt(operands[o++]);
+                } else if (operators[i].equals("!")) {
+                    factorial = factorial(factorial);
+                    action = factorial;
+                }
 
-        String operators[] = input.split("[0-9]");
-        String operands[] = input.split("[*+-/!]");
-        int action = Integer.parseInt(operands[0]);
-        int o = 1;
-        for (int i = 0; i < operators.length; i++) {
-            if (operators[i].equals("+")) {
-                action += Integer.parseInt(operands[o++]);
             }
-            else if (operators[i].equals("-")) {
-                action -= Integer.parseInt(operands[o++]);
-            }
-            else if (operators[i].equals("*")) {
-                action *= Integer.parseInt(operands[o++]);
-            }
-            else if (operators[i].equals("/")) {
-                action /= Integer.parseInt(operands[o++]);
-            }
-            else if (operators[i].equals("!")) {
-                action = factorial(action);
-                o++;
-            }
-
+            return action;
         }
-        return action;
     }
-
-    private static boolean empty(String string) {
-        if (string.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
-
     public static int factorial (int number) {
         int factorial = 1;
         for (int i = 1; i < number+1; i++) {
@@ -53,4 +45,13 @@ class Calculator{
         }
         return factorial;
     }
+
+    public static void printArray (String [] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+
+
 }
