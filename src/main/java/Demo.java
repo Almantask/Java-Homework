84138a87d983b7a901e1bb143ef4fffcfd2b4009
Artur <artur.tulupov@gmail.com>
2020-11-05 21:13:26 +0200

    PR test/base ground for Cooking HW

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,4 @@
-package main.java;
-
 public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { System.out.println("Hello world!");
     }
 }
