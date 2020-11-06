e2f67676380adccb9104e1e49d626e5731ff6f2b
rimux9 <rmatuliokas@gmail.com>
2020-11-06 14:28:29 +0200

    cooking

---
+++ src/main/java/CookingMeasurement.java
@@ -0,0 +1,21 @@
+public class CookingMeasurement {
+
+    private float amount;
+    private String unit;
+
+    public CookingMeasurement(float amount, String unit) {
+
+        this.amount = amount;
+        this.unit = unit;
+
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public String getUnit() {
+        return unit;
+
+    }
+}
