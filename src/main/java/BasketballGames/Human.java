30a7eb000201fea648209a7be47f7ee8a1ce5646
Renata Kazragyte <777akata777@gmail.com>
2020-11-08 13:30:15 +0200

    basketball game -starting

--- src/main/java/BasketballGames/Human.java
+++ src/main/java/BasketballGames/Human.java
@@ -7,12 +7,12 @@ public class Human {
     //    ### 0- Human (EASY):very member of a team has a name, birthday, age, nationality.
     private String name;
     private LocalDate birthday;
-    private int age;
-    private String nationality;
+        private String nationality;
 
-    public static Human(String name, LocalDate birthday, int age, String nationality) {
+    public static Human(String name, LocalDate birthday, String nationality) {
         this.name = name;
         this.birthday = birthday;
+        this.nationality = nationality;
 
     }
 
