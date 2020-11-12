e58750ab962bb7468db466760aa8ca7e8412e480
Almantas Karpavičius <almantusk@gmail.com>
2020-11-12 22:53:53 +0200

    cleaning up?

---
+++ src/main/java/edu/Demo.java
@@ -0,0 +1,7 @@
+package edu;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/java/edu/Foo.java
@@ -0,0 +1,11 @@
+package edu;
+
+public class Foo {
+    /**
+     *
+     * @return 1
+     */
+    public static int bar(){
+        return 1;
+    }
+}
