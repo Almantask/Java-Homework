18c2efec2e2e600bd68322f37124122405188914
Jolita-byte <59626210+Jolita-byte@users.noreply.github.com>
2020-11-14 22:35:36 +0200

    Slowest Key Press Task

---
+++ src/main/java/edu/Main.java
@@ -0,0 +1,13 @@
+package edu;
+
+public class Main {
+    public static void main(String[] args) {
+        SlowestKeySolution a = new SlowestKeySolution();
+        int[] releaseTimes = {1,2,3};
+        String keysPressed = "abc";
+        char b = SlowestKeySolution.solve(releaseTimes, keysPressed);
+        System.out.println(b);
+
+        //System.out.println(keysPressed.("[]abc"));
+    }
+}
