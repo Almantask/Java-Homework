7edfd937a33d1bb6bc1830325daa7922130a6421
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-11-15 01:48:59 +0200

    Slowest Key. All tests passed.

--- src/main/java/edu/Demo.java
+++ src/main/java/edu/Demo.java
@@ -2,6 +2,13 @@ package edu;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        int[] releaseTimes = {12,23,36,46,62};
+        String keysPressed = "spuda";
+
+        char slowestKey = SlowestKeySolution.solve(releaseTimes,keysPressed);
+
+        System.out.println(slowestKey);
+
     }
 }
