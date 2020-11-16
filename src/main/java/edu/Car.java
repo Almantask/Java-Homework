94e398d6209e66ec428de646367be5f29ab89e1a
Jolita-byte <59626210+Jolita-byte@users.noreply.github.com>
2020-11-16 22:46:52 +0200

    Alarm system task

--- src/main/java/edu/Car.java
+++ src/main/java/edu/Car.java
@@ -1,22 +1,34 @@
 package edu;
 
 public class Car {
+    private Alarm alarm;
+    private Lights lights;
+    private boolean isLocked;
+
     public Car(Alarm alarm, Lights lights) {
+        this.alarm = alarm;
+        this.lights = lights;
     }
 
     public boolean isLocked(){
-        return false;
+        return isLocked;
     }
 
     public void lock(){
-
+        if (!isLocked){
+            isLocked = true;
+        }
     }
 
     public void unlock(){
-
+        if (isLocked){
+            isLocked = false;
+        }
     }
 
     public void open(){
-
+        if (isLocked){
+            alarm.beep();
+        }
     }
 }
