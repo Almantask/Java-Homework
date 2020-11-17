6b6b9b7ad8327b23cbb35c47599bfd71a07517b5
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-11-17 04:06:48 +0200

    Minimum deletion cost

--- src/main/java/edu/MinCostSolution.java
+++ src/main/java/edu/MinCostSolution.java
@@ -1,7 +1,27 @@
 package edu;
 
+import java.util.Arrays;
+
 public class MinCostSolution {
-    public static int solve(String s, int[] cost){
-        return 0;
+
+    public static int solve(String s, int[] cost) {
+
+        String[] stringElements = s.split("(?<=(.))(?!\\1)");
+
+        int result = 0;
+        int start = 0;
+        for (String value : stringElements) {
+            int end = start + value.length();
+            result = result + costSum(cost, start, end);
+            start = start + value.length();
+        }
+        return result;
+    }
+
+    private static int costSum(int[] cost, int start, int end) {
+        int[] costArray = Arrays.copyOfRange(cost, start, end);
+        int sum = Arrays.stream(costArray).sum();
+        int max = Arrays.stream(costArray).max().getAsInt();
+        return sum - max;
     }
 }
