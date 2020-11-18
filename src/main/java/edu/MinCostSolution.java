49c7dea21839aec99c4dfd7607ba09459449c330
Edvinas <edvinasalimas98@gmail.com>
2020-11-18 17:53:31 +0200

    Min cost solution v 1.0

--- src/main/java/edu/MinCostSolution.java
+++ src/main/java/edu/MinCostSolution.java
@@ -1,7 +1,23 @@
 package edu;
 
 public class MinCostSolution {
-    public static int solve(String s, int[] cost){
-        return 0;
+
+    public static final char INVALID = '-';
+
+    public static int solve(String key, int[] numbers ) {
+        if (numbers == null || numbers == null || key.isEmpty() || numbers.length != key.length() ) {
+            return INVALID;
+        }
+        int trackedNumbers = numbers[0];
+        int maxCost = numbers[0];
+        for (int i = 1; i < key.length(); ++i) {
+            if (key.charAt(i) != key.charAt(i - 1)) {
+                trackedNumbers -= maxCost;
+                maxCost = 0;
+            }
+            trackedNumbers += numbers[i];
+            maxCost = Math.max(maxCost, numbers[i]);
+        }
+        return trackedNumbers - maxCost;
     }
 }
--- src/test/java/edu/tests/MinCostSolutionTests.java
+++ src/test/java/edu/tests/MinCostSolutionTests.java
@@ -1,4 +1,42 @@
 package edu.tests;
 
+import edu.MinCostSolution;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MinCostSolutionTests {
+
+    @Test
+    @DisplayName("Check for one pair of letters")
+    public void check_ForOnlyOnePair_ofLetters () {
+        String key = "abaac";
+        int [] cost = {1,2,3,4,5};
+
+        int answer = MinCostSolution.solve(key,cost);
+
+        assertEquals(3, answer);
+    }
+
+    @Test
+    @DisplayName("Check for zero pairs of letters")
+    public void check_for_nonePairs() {
+        String key = "abc";
+        int [] cost = {1,2,3};
+
+        int answer = MinCostSolution.solve(key,cost);
+
+        assertEquals(0, answer);
+    }
+
+    @Test
+    @DisplayName("Check for two pairs of letters")
+    public void checkFor_two_or_morePairs () {
+        String key = "aabaa";
+        int [] cost = {1,2,3,4,1};
+
+        int answer = MinCostSolution.solve(key,cost);
+
+        assertEquals(2, answer);
+    }
 }
