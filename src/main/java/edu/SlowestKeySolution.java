fdd1cb549a36b90db9a7d07925277f9a2f13ec2a
Rustamas <rustams@live.com>
2020-11-18 16:12:57 +0200

    Slowest Key Solution

--- src/main/java/edu/SlowestKeySolution.java
+++ src/main/java/edu/SlowestKeySolution.java
@@ -6,4 +6,24 @@ public class SlowestKeySolution {
     public static char solve(int[] releaseTimes, String keysPressed) {
         return INVALID;
     }
+
+    class Solution {
+        public char slowestKey(int[] releaseTimes, String keysPressed) {
+            char slowestKey = keysPressed.charAt(0);
+            int slowestKeyDuration = releaseTimes[0];
+
+            for (int i = 1; i < keysPressed.length(); i++) {
+                char c = keysPressed.charAt(i);
+                int duration = releaseTimes[i] - releaseTimes[i - 1];
+                if (duration > slowestKeyDuration
+                        || (duration == slowestKeyDuration && c > slowestKey)) {
+                    slowestKey = c;
+                    slowestKeyDuration = duration;
+                }
+            }
+            return slowestKey;
+        }
+    }
+
+
 }
