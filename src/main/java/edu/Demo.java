194ca7e4cb7b59596ac8e138fc92c69e1edd2751
Dommiiii11 <domilegotovtaite@gmail.com>
2020-11-19 10:13:06 +0200

    All tests passed and deleted sysout. Deleted main.

--- src/main/java/edu/Demo.java
+++ src/main/java/edu/Demo.java
@@ -3,8 +3,6 @@ package edu;
 public class Demo {
 
         public static void main(String[] args) {
-        String keysPressed = "cbcd";
-        int[] releaseTimes = new int[] {9, 29, 49, 50};
-        SlowestKeySolution.solve(releaseTimes, keysPressed);
+
     }
 }
