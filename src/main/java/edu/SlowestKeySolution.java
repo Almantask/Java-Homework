b66cf8d851e39dacfa7fe90a52325ad8bdbca613
d3n0 <deivis.norvaisas@gmail.com>
2020-11-21 17:35:32 +0200

    Slowest Key Solution 3rd try. Finally tests passed!

--- src/main/java/edu/SlowestKeySolution.java
+++ src/main/java/edu/SlowestKeySolution.java
@@ -4,52 +4,67 @@ import java.util.ArrayList;
 
 public class SlowestKeySolution {
     public static final char INVALID = '-';
-    public static char longestPressedCharacter;
+    public static ArrayList<Character> uniqueKeysPressed = new ArrayList<>();
 
     public static char solve(int[] releaseTimes, String keysPressed) {
+        uniqueKeysPressed.clear();
 
 
-
-        if (releaseTimes == null || releaseTimes.length < 1 || !keysPressed.matches("[a-z]+") || releaseTimes.length != keysPressed.length())
+        if (releaseTimes == null || releaseTimes.length < 1 || keysPressed == null || keysPressed.matches("[^a-z]+") || releaseTimes.length != keysPressed.length())
             return INVALID;
+
 
         int[] durations = new int[releaseTimes.length];
         durations[0] = releaseTimes[0];
         for (int i=1; i<durations.length; i++) {
             durations[i] = releaseTimes[i]-releaseTimes[i-1];
         }
-        ArrayList<Integer> duplicatingIndexes = findDuplications(durations);
 
-        if(duplicatingIndexes.size() > 0) {
-            longestPressedCharacter = keysPressed.charAt(duplicatingIndexes.get(0));
-        } else {
-            for(int i=0;i<durations.length;i++){
-                if(durations[i] == getHighest(durations))
-                    longestPressedCharacter = keysPressed.charAt(i);
-            }
-        }
-        return longestPressedCharacter;
+        int[] longestPressedChar = findDuplicates(durations, keysPressed);
+        return uniqueKeysPressed.get(longestPressedChar[1]);
+
+        //return longestPressedCharacter;
     }
-    private static ArrayList<Integer> findDuplications(int[] data) {
-        ArrayList<Integer> duplicatingIndexes = new ArrayList<Integer>();
-        int highestInteger = getHighest(data);
-        for(int i=0;i<data.length;i++) {
-            if(data[i] == highestInteger) {
-                for(int j=i+1;j<data.length;j++) {
-                    if(data[i] == data[j]) {
-                        duplicatingIndexes.add(i);
-                        duplicatingIndexes.add(j);
-                    }
+
+    private static int[] findDuplicates(int[] durations, String keysPressed) {
+        char[] keysPressedArr = new char[keysPressed.length()];
+        keysPressedArr = keysPressed.toCharArray();
+
+        for (int i=0; i<keysPressedArr.length;i++) {
+            int k=0;
+            for(int j=0;j<uniqueKeysPressed.size();j++) {
+                if(uniqueKeysPressed.get(j) == keysPressedArr[i]) {
+                    k++;
                 }
             }
+            if(k == 0) {
+                uniqueKeysPressed.add(keysPressedArr[i]);
+            }
         }
-        return duplicatingIndexes;
+
+        int[] finalDurations = new int[uniqueKeysPressed.size()];
+
+        if(keysPressedArr.length == uniqueKeysPressed.size()) {
+            return getHighest(durations);
+        }
+
+        for(int j=0;j<keysPressedArr.length;j++) {
+            for(int i=0;i<uniqueKeysPressed.size();i++) {
+                if(uniqueKeysPressed.get(i) == keysPressedArr[j]) {
+                    finalDurations[i] += durations[j];
+                }
+
+            }
+        }
+        return getHighest(finalDurations);
     }
-    static int getHighest(int[] data) {
-        int highestNumber = 0;
+
+    static int[] getHighest(int[] data) {
+        int[] highestNumber = new int[2];
         for(int i=0;i<data.length;i++) {
-            if(data[i] > highestNumber) {
-                highestNumber = data[i];
+            if(data[i] > highestNumber[0]) {
+                highestNumber[0] = data[i];
+                highestNumber[1] = i;
             }
         }
         return highestNumber;
