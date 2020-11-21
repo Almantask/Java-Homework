c2450737f8515a318aec6c3d2547a4e2c7cf7fae
d3n0 <deivis.norvaisas@gmail.com>
2020-11-21 20:28:31 +0200

    I don't understand the tests.

--- src/main/java/edu/MinCostSolution.java
+++ src/main/java/edu/MinCostSolution.java
@@ -2,6 +2,46 @@ package edu;
 
 public class MinCostSolution {
     public static int solve(String s, int[] cost){
-        return 0;
+        if(!validate(s, cost))
+            return 0;
+
+        return calculateCost(s, cost);
+
+    }
+
+    private static boolean validate(String s, int[] cost) {
+        if(!s.matches("[a-z]+") || s.length() <= 1 || cost.length > (10^5) )
+            return false;
+
+        for(int i=0;i< cost.length;i++) {
+            if(cost[i] < 1 || cost[i] > (10^4))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean identicalLettersContaining(char[] extractedChars) {
+
+        for(int i=0;i<extractedChars.length;i++) {
+            if(extractedChars[i] == extractedChars[i+1])
+                return true;
+        }
+        return false;
+    }
+
+    private static int calculateCost(String str, int[] costAt) {
+        char[] extractedChars = str.toCharArray();
+        int cost = 0;
+
+        if(!identicalLettersContaining(extractedChars)) {
+            return 0;
+        }
+
+        for(int i=0;i<extractedChars.length;i++) {
+            if(extractedChars[i] == extractedChars[i+1]) {
+                cost += Math.min(extractedChars[i], extractedChars[i+1]);
+            }
+        }
+        return cost;
     }
 }
--- src/test/java/edu/tests/MinCostSolutionTests.java
+++ src/test/java/edu/tests/MinCostSolutionTests.java
@@ -1,4 +1,18 @@
 package edu.tests;
 
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class MinCostSolutionTests {
+
+    // test function validate
+    // input non-english letters expected output false
+    // input string length less than two must output false
+    // input cost length larger than 10^5 must return false
+    // input any of cost[i] length more than 10^4 must return false
+    // otherwise must validate and return true
+    // input (String, int[])
+
+
 }
