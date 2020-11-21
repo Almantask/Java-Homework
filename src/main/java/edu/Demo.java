2ad0a61e402eef410647be40849038d1a80da15b
Almantas Karpavičius <almantusk@gmail.com>
2020-11-21 09:59:00 +0200

    ProgressBar example to demo encapsulation and a little bit of abstraction

--- src/main/java/edu/Demo.java
+++ src/main/java/edu/Demo.java
@@ -2,6 +2,17 @@ package edu;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        ProgressBar bar = new ProgressBar(0, 100);
+        for (int i = 0; i < 100; i++) {
+            bar.addProgress(1);
+            clearScreen();
+            System.out.println(bar);
+        }
+    }
+
+    // TODO: figure out how to clear a console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
---
+++ src/main/java/edu/ProgressBar.java
@@ -0,0 +1,50 @@
+package edu;
+
+// This class demonstrates encapsulation
+// We hid the internal ProgressBar details
+// And exposed all the functionality through methods
+// This also demonstrates abstraction
+// ProgressBar is a concept: can increase progress, can print progress
+public class ProgressBar {
+    // Assume process can only increase
+    private float currentProgress;
+    private float max;
+
+    public ProgressBar(float currentProgress, float max) {
+        if(currentProgress >= 0){
+            this.currentProgress = currentProgress;
+        }
+        if(max > 0){
+            this.max = max;
+        }
+        else{
+            // default max
+            this.max = 100;
+        }
+    }
+
+    public void addProgress(float increment){
+        if(increment < 0) return;
+
+        currentProgress+=increment;
+        if(currentProgress > max){
+            currentProgress = max;
+        }
+    }
+
+    public float getProgress(){
+        return currentProgress / max;
+    }
+
+    @Override
+    public String toString() {
+        String progress = "";
+        float tenPercent = max * 0.1f;
+        int stars = (int)(currentProgress / tenPercent);
+        for (int i = 0; i < stars; i++) {
+            progress +="*";
+        }
+
+        return progress;
+    }
+}
