c796c98a62c30806860de24bcf9ad3b44af2af8d
Edvinas <edvinasalimas98@gmail.com>
2020-11-29 14:44:59 +0200

    Tests v 1.1

--- tests/CookingConvertor/CookingMeasurementConverterTest.java
+++ tests/CookingConvertor/CookingMeasurementConverterTest.java
@@ -1,8 +1,7 @@
 package CookingConvertor;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CookingMeasurementConverterTest {
 
