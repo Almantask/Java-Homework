589eea9cc79ca20c6ad773b2e9f57b0f70043d13
Edvinas <edvinasalimas98@gmail.com>
2020-12-12 13:43:23 +0200

    Bank Account transaction v1,0

--- src/main/java/edu/BankAccount.java
+++ src/main/java/edu/BankAccount.java
@@ -4,12 +4,12 @@ public class BankAccount{
 
     private double balance;
 
-    public BankAccount() {
-        this.balance = 0;
+    public BankAccount(int balance) {
+        this.balance = balance;
     }
 
     public void transferMoneyTo (BankAccount bankAccount, double amount) {
-        bankAccount.balance += amount;
+            bankAccount.balance += amount;
     }
 
     public void setBalance(double balance) {
--- src/main/java/edu/Demo.java
+++ src/main/java/edu/Demo.java
@@ -1,18 +1,25 @@
 package edu;
 
-public class Demo {
-    public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.setBalance(10000000.0);
-        BankAccount bankAccount1 = new BankAccount();
-        bankAccount.setBalance(0);
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-        for (int i = 0; i <= 1000; i++) {
+public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount bankAccount = new BankAccount(10000000);
+        BankAccount bankAccount1 = new BankAccount(0);
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+
             Thread newThread = new Thread(() -> {
-                bankAccount.transferMoneyTo(bankAccount1, 100);
-                System.out.println(bankAccount1.getBalance());
+                    for (int i = 0; i <= 1000; i++) {
+                        bankAccount.transferMoneyTo(bankAccount1, 100);
+                        System.out.println(bankAccount1.getBalance());
+                    }
             });
-            newThread.start();
+            executorService.submit(newThread);
+            executorService.awaitTermination(2 , TimeUnit.MILLISECONDS);
+            executorService.shutdown();
         }
     }
-}
+
+
