fd88609979da3118588edd7756cc8b933503d504
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-14 00:56:35 +0200

    Advent Of Code Day5

--- src/main/java/Advent/Day5.java
+++ src/main/java/Advent/Day5.java
@@ -1,24 +1,20 @@
 package Advent;
 
-import org.graalvm.compiler.serviceprovider.IsolateUtil;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class Day5 {
-    public static int solveDay5(List input) {
+    public static int solveDay5(List<String> input) {
         List<Integer> listOfID = new ArrayList<>();
         for (Object o : input) {
             listOfID.add(findSeatID(o));
         }
         System.out.println(listOfID);
-        int max = Collections.max(listOfID);
-        return max;
+        return Collections.max(listOfID);
     }
 
-    public static int findYourSeat (List input){
+    public static int findYourSeat (List<String> input){
         List<Integer> listOfID = new ArrayList<>();
         for (Object o : input) {
             listOfID.add(findSeatID(o));
@@ -40,27 +36,24 @@ public class Day5 {
         char[] seatsCode = o.toString().toCharArray();
         for (char c : seatsCode) {
             if (c == 'F' || c == 'B') {
-                minMaxRow = changeLimits(c, minMaxRow);
+                changeLimits(c, minMaxRow);
             } else if (c == 'L' || c == 'R') {
-                minMaxCol = changeLimits(c, minMaxCol);
+                changeLimits(c, minMaxCol);
             }
         }
         return minMaxRow[1] * 8 + minMaxCol[1];
     }
 
-    private static int[] changeLimits(char ch, int[] minMax) {
+    private static void changeLimits(char ch, int[] minMax) {
         switch (ch) {
             case 'F':
             case 'L':
-                minMax[0] = minMax[0];
                 minMax[1] -= ((minMax[1] - minMax[0]) + 1) / 2;
                 break;
             case 'B':
             case 'R':
                 minMax[0] += ((minMax[1] - minMax[0]) + 1) / 2;
-                minMax[1] = minMax[1];
                 break;
         }
-        return minMax;
     }
 }
