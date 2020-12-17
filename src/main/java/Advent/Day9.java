39ed887a2b1f5a174316f70107ef384e8635723d
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-17 04:59:55 +0200

    Advent Of Code Day9

---
+++ src/main/java/Advent/Day9.java
@@ -0,0 +1,50 @@
+package Advent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day9 {
+
+    public static void solve(List<String> input) {
+
+        System.out.println("Answer of Day9 part 1 is: " + findFirstNotMatchedNumber(input));
+        System.out.println();
+        System.out.println("Answer of Day9 part 2 is: " + encryptedWeakness(input));
+    }
+
+    private static long encryptedWeakness (List<String> input) {
+        List<Long> numbers = new ArrayList<>();
+        long number = 0;
+        long goal = 50047984;
+        int i = 0;
+        int secondLoop = 0;
+        while (true){
+            if (number > goal) break;
+            numbers.add(Long.parseLong(input.get(i)));
+            number += Long.parseLong(input.get(i));
+            i += 1;
+            if(number == goal) break;
+            if(number > goal){
+                i = secondLoop + 1;
+                secondLoop += 1;
+                number = 0;
+                numbers.removeAll(numbers);
+            }
+        }
+return Collections.max(numbers) + Collections.min(numbers);
+    }
+
+    private static long findFirstNotMatchedNumber(List<String> input) {
+        List<Long> number = new ArrayList<>();
+        for (int i = 25; i < input.size(); i++) {
+            number.add(Long.parseLong(input.get(i)));
+            for (int j = i - 25; j < i; j++)
+                for (int k = i - 24; k < i - 1; k++)
+                    if ((Long.parseLong(input.get(j)) != (Long.parseLong(input.get(k)))) &&
+                            Long.parseLong(input.get(j)) + Long.parseLong(input.get(k))
+                                    == Long.parseLong(input.get(i))) number.remove(Long.parseLong(input.get(i)));
+        }
+        return number.get(0);
+    }
+}
--- src/main/java/main/java/Demo.java
+++ src/main/java/main/java/Demo.java
@@ -1,17 +1,16 @@
 package main.java;
 
-import Advent.Day7;
-import Advent.Day8;
+import Advent.Day9;
 import Advent.Input;
 import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\Augis\\Desktop\\JAVA\\Advent Input\\Day8.txt";
-        List input = Input.readFromFile(path);
+        String path = "C:\\Users\\Augis\\Desktop\\JAVA\\Advent Input\\Day9.txt";
+        List<String> input = Input.readFromFile(path);
 
-        Day8.solve(input);
+        Day9.solve(input);
 
     }
 }
