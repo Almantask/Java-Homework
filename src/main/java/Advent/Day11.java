c1154d433d800de5f0f1134780f02ea4c5ccf28b
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-18 22:33:31 +0200

    Advent Of Code Day11

--- src/main/java/Advent/Day11.java
+++ src/main/java/Advent/Day11.java
@@ -8,7 +8,66 @@ public class Day11 {
     public static void solve(List<String> input) {
 
         System.out.println("The answer of Day 11 part 1 is : " + countAllOccupiedSeat(input));
+        System.out.println();
+        System.out.println("The answer of Day 11 part 2 is : " + countAllOccupiedSeat2(input));
+    }
 
+    private static int countFirstSeenOccupiedSeat(List<String> strings, int seatRow, int seatNumber) {
+        int sumOccupiedSeat = 0;
+        char occupiedSeat = '#';
+        int[] firstSeat = {-1, -1};
+        int[] secondSeat = {-1, 0};
+        int[] thirdSeat = {-1, 1};
+        int[] fourthSeat = {0, -1};
+        int[] fifthSeat = {0, 1};
+        int[] sixthSeat = {1, -1};
+        int[] seventhSeat = {1, 0};
+        int[] eighthSeat = {1, 1};
+        List<int[]> instructions = new ArrayList<>();
+        instructions.add(firstSeat);
+        instructions.add(secondSeat);
+        instructions.add(thirdSeat);
+        instructions.add(fourthSeat);
+        instructions.add(fifthSeat);
+        instructions.add(sixthSeat);
+        instructions.add(seventhSeat);
+        instructions.add(eighthSeat);
+        for (int[] ints : instructions) {
+            char floor = '.';
+            int row = seatRow + ints[0];
+            int col = seatNumber + ints[1];
+            char seenSeat = strings.get(row).toCharArray()[col];
+            while (row > 0 && col > 0 &&
+                    row < strings.size() && col < strings.get(row).length() &&
+                    seenSeat == floor) {
+                seenSeat = strings.get(row).toCharArray()[col];
+                row += ints[0];
+                col += ints[1];
+            }
+            if (seenSeat == occupiedSeat)
+                sumOccupiedSeat += 1;
+        }
+        return sumOccupiedSeat;
+    }
+
+    private static int countAllOccupiedSeat2(List<String> input) {
+        List<String> map = getChangedMap(input);
+        int seatNumber1 = 0;
+        int seatNumber2 = 1;
+        while (seatNumber1 != seatNumber2) {
+            List<String> changedMap = new ArrayList<>(map);
+            seatNumber1 = countOccupiedSeat(map);
+            for (int i = 1; i < changedMap.size() - 1; i++) {
+                char[] seats = changedMap.get(i).toCharArray();
+                for (int j = 1; j < seats.length - 1; j++) {
+                    if (checkIfNeedToChangeState2(seats[j], countFirstSeenOccupiedSeat(changedMap, i, j)))
+                        seats[j] = changeSeatState(seats[j]);
+                }
+                map.set(i, String.valueOf(seats));
+            }
+            seatNumber2 = countOccupiedSeat(map);
+        }
+        return seatNumber2;
     }
 
     private static int countAllOccupiedSeat(List<String> input) {
@@ -27,6 +86,7 @@ public class Day11 {
                 map.set(i, String.valueOf(seats));
             }
             seatNumber2 = countOccupiedSeat(map);
+
 //            for(Object o : map)
 //                System.out.println(o);
         }
@@ -51,6 +111,14 @@ public class Day11 {
         if (ch == empty && occupiedAroundSeat == 0)
             return true;
         return ch == occupied && occupiedAroundSeat >= 4;
+    }
+
+    private static boolean checkIfNeedToChangeState2(char ch, int occupiedAroundSeat) {
+        char empty = 'L';
+        char occupied = '#';
+        if (ch == empty && occupiedAroundSeat == 0)
+            return true;
+        return ch == occupied && occupiedAroundSeat >= 5;
     }
 
     private static int countOccupiedAroundSeat(List<String> strings, int seatRow, int seatNumber) {
