05011bc67149f1789962fcbfb65e0cd4a0416ef1
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-20 17:11:05 +0200

    Advent Of Code Day13

--- src/main/java/Advent/Day13.java
+++ src/main/java/Advent/Day13.java
@@ -8,10 +8,54 @@ public class Day13 {
 
     public static void solve(List<String> input) {
 
-        System.out.println(input);
         System.out.println();
-        System.out.println(solvePart1(input));
+        System.out.println("The answer of Day13 part1 is: " + solvePart1(input));
+        System.out.println();
+        solvePart2(input);
 
+    }
+
+    private static long solvePart2(List<String> input) {
+        List<int[]> busIDsAndDepartDif = getBusIDAndDepartDifferences(input);
+        long maxBusID = 0; // 310357331l
+        long timeDifferent = 0;
+        for(int[] o : busIDsAndDepartDif)
+            if(o[0] > maxBusID){
+                maxBusID = o[0];
+                timeDifferent = o[1];
+            }
+        System.out.println("maxBusID = " + maxBusID);
+        System.out.println(timeDifferent);
+        long timestamp = maxBusID - timeDifferent; // 19380331813222l
+        long timestamp2 = 0;
+        while (timestamp > timestamp2) {
+            timestamp2 = timestamp;
+            int check = 0;
+            for (int[] o : busIDsAndDepartDif) {
+                long a = (timestamp + o[1]) / o[0];
+                if ((a*o[0]-o[1]) == timestamp)
+                    continue;
+                check += 1;
+            }
+            if (check > 0)
+                timestamp += maxBusID;
+            System.out.println("timestamp = " + timestamp);
+        }
+        System.out.println(input);
+        return timestamp;
+    }
+
+    private static List<int[]> getBusIDAndDepartDifferences(List<String> input) {
+        List<int[]> busID_timeDepart = new ArrayList<>();
+        List<String> busIDs = new ArrayList(Arrays.asList(input.get(1).split(",")));
+        for (int i = 0; i < busIDs.size(); i++) {
+            if (busIDs.get(i).equals("x"))
+                continue;
+            int busID = Integer.parseInt(busIDs.get(i));
+            int[] idAndTime = {busID, i};
+            busID_timeDepart.add(idAndTime);
+        }
+        return busID_timeDepart;
     }
 
     private static int solvePart1(List<String> input) {
@@ -29,7 +73,7 @@ public class Day13 {
                 busID = busNumber;
             }
         }
-        System.out.println("time: " + time + ", waitingTime: " + waitingTime + ", busID: " + busID);
+//        System.out.println("time: " + time + ", waitingTime: " + waitingTime + ", busID: " + busID);
         return busID * waitingTime;
     }
 }
