ac0778b7e3ceb11085a28922d069185903bfdd1b
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-23 18:37:59 +0200

    Advent Of Code Day2

--- src/main/java/Advent/Day2.java
+++ src/main/java/Advent/Day2.java
@@ -6,81 +6,74 @@ import java.util.Objects;
 
 public class Day2 {
 
-    public static int solveDay2(List input) {
+    public static void solve(List<String> input) {
+        System.out.println("The answer of Day2 part1 is: " + solvePart1(input));
+        System.out.println();
+        System.out.println("The answer of Day2 part2 is: " + solvePart2(input));
+    }
 
-        List limits = limitsList(input);
-        List letters = lettersList(input);
-        List codes = codesList(input);
-
-        int sumValidPasswords= 0;
+    public static int solvePart1(List<String> input) {
+        List<int[]> limits = limitsList(input);
+        char[] letters = lettersList(input);
+        List<String> codes = codesList(input);
+        int sumValidPasswords = 0;
         for (int i = 0; i < limits.size(); i++) {
-            if (passwordsValidation(limits.get(i).toString(), letters.get(i).toString(), codes.get(i).toString()) != true) {
+            if (!passwordsValidation(limits.get(i), letters[i], codes.get(i)))
                 continue;
-            }
-            sumValidPasswords ++;
+            sumValidPasswords++;
         }
         return sumValidPasswords;
     }
 
-    public static int solveDay2_2(List input) {
-
-        List limits = limitsList(input);
-        List letters = lettersList(input);
-        List codes = codesList(input);
-
-        int sumValidPasswords2 = 0;
+    public static int solvePart2(List<String> input) {
+        List<int[]> limits = limitsList(input);
+        char[] letters = lettersList(input);
+        List<String> codes = codesList(input);
+        int sumValidPasswords = 0;
         for (int i = 0; i < limits.size(); i++) {
-            if (passwordsValidation2(limits.get(i).toString(), letters.get(i).toString(), codes.get(i).toString()) != true) {
+            if (!passwordsValidation2(limits.get(i), letters[i], codes.get(i))) {
                 continue;
             }
-            sumValidPasswords2 ++;
+            sumValidPasswords++;
         }
-        return sumValidPasswords2;
+        return sumValidPasswords;
     }
 
-    private static boolean passwordsValidation(String limit, String letter, String code) {
-        int countLettersInCode = code.length() - code.replace(letter, "").length();
-        return countLettersInCode >= min(limit) && countLettersInCode <= max(limit);
+    private static boolean passwordsValidation(int[] limit, char letter, String code) {
+        int countLettersInCode = code.length() - code.replace(String.valueOf(letter), "").length();
+        return countLettersInCode >= limit[0] && countLettersInCode <= limit[1];
     }
 
-    private static boolean passwordsValidation2 (String limit, String letter, String code){
-        char first = code.charAt(min(limit)-1);
-        char second = code.charAt(max(limit)-1);
-        char letter2= letter.charAt(0);
-        return (Objects.equals(first, letter2) && !Objects.equals(second, letter2)) || (!Objects.equals(first, letter2) && Objects.equals(second, letter2));
+    private static boolean passwordsValidation2(int[] limit, char letter, String code) {
+        char first = code.charAt(limit[0] - 1);
+        char second = code.charAt(limit[1] - 1);
+        return (Objects.equals(first, letter) && !Objects.equals(second, letter))
+                || (!Objects.equals(first, letter) && Objects.equals(second, letter));
     }
 
-    private static List<String> limitsList(List<String> strings) {
-        List<String> limits = new ArrayList<>();
-        for (int i = 0; i < strings.size() / 3; i++) {
-            limits.add(strings.get(i * 3));
+    private static List<int[]> limitsList(List<String> input) {
+        List<int[]> limits = new ArrayList<>();
+        for (String s : input) {
+            int[] minAndMax = new int[2];
+            String limit = s.split(" ")[0];
+            minAndMax[0] = Integer.parseInt(limit.split("-")[0]);
+            minAndMax[1] = Integer.parseInt(limit.split("-")[1]);
+            limits.add(minAndMax);
         }
         return limits;
     }
 
-    private static int min(String string) {
-        String[] limits = string.split("-");
-        return Integer.parseInt(limits[0]);
-    }
-
-    private static int max(String string) {
-        String[] limits = string.split("-");
-        return Integer.parseInt(limits[1]);
-    }
-
-    private static List<Character> lettersList(List<String> strings) {
-        List<Character> letters = new ArrayList<Character>();
-        for (int i = 0; i < strings.size() / 3; i++) {
-            letters.add(strings.get((i * 3) + 1).charAt(0));
-        }
+    private static char[] lettersList(List<String> input) {
+        char[] letters = new char[input.size()];
+        for (int i = 0; i < input.size(); i++)
+            letters[i] = input.get(i).charAt(input.get(i).indexOf(":") - 1);
         return letters;
     }
 
-    private static List<String> codesList(List<String> strings) {
-        ArrayList<String> codes = new ArrayList<String>();
-        for (int i = 0; i < strings.size() / 3; i++) {
-            codes.add(strings.get((i * 3) + 2));
-        }
+    private static List<String> codesList(List<String> input) {
+        List<String> codes = new ArrayList<>();
+        for (String s : input)
+            codes.add(s.split(" ")[2]);
         return codes;
     }
 }
