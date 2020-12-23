8eae1f82ca148b70f31d6d01eb10a91f26addefb
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-23 15:41:40 +0200

    Advent Of Code Day1

--- src/main/java/Advent/Day1.java
+++ src/main/java/Advent/Day1.java
@@ -4,13 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Day1 {
-    public static int solveDay1(List input) {
 
+    public static void solve (List<String> input){
+
+        System.out.println("The answer of Day1 part 1 is: " + part1(input));
+        System.out.println();
+        System.out.println("The answer of Day1 part 2 is: " + part2(input));
+    }
+
+    private static int part2 (List<String> input) {
         int [] ints = new int[input.size()];
-        Arrays.setAll(ints, i -> Integer.parseInt(input.get(i).toString()));
-
+        Arrays.setAll(ints, i -> Integer.parseInt(input.get(i)));
         int multiplying = 0;
-
         for (int i = 0; i < ints.length - 1; i++) {
             for (int j = 1; j < ints.length; j++) {
                 for (int k = 2; k < ints.length; k++) {
@@ -22,4 +27,19 @@ public class Day1 {
         }
         return multiplying;
     }
+
+    private static int part1 (List<String> input) {
+        int [] ints = new int[input.size()];
+        Arrays.setAll(ints, i -> Integer.parseInt(input.get(i)));
+        int multiplying = 0;
+        for (int i = 0; i < ints.length - 1; i++) {
+            for (int j = 1; j < ints.length; j++) {
+                    if (ints[i] + ints[j] == 2020) {
+                        multiplying = ints[i] * ints[j];
+                    }
+                }
+        }
+        return multiplying;
+    }
+
 }
