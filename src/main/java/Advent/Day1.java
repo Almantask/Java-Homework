c4566dec132ef490e5797be6e5b173420092468b
Augis08 <73080958+Augis08@users.noreply.github.com>
2020-12-23 15:46:58 +0200

    Advent Of Code Day1

--- src/main/java/Advent/Day1.java
+++ src/main/java/Advent/Day1.java
@@ -5,41 +5,33 @@ import java.util.List;
 
 public class Day1 {
 
-    public static void solve (List<String> input){
+    public static void solve(List<String> input) {
 
         System.out.println("The answer of Day1 part 1 is: " + part1(input));
         System.out.println();
         System.out.println("The answer of Day1 part 2 is: " + part2(input));
     }
 
-    private static int part2 (List<String> input) {
-        int [] ints = new int[input.size()];
+    private static int part2(List<String> input) {
+        int[] ints = new int[input.size()];
         Arrays.setAll(ints, i -> Integer.parseInt(input.get(i)));
         int multiplying = 0;
-        for (int i = 0; i < ints.length - 1; i++) {
-            for (int j = 1; j < ints.length; j++) {
-                for (int k = 2; k < ints.length; k++) {
-                    if (ints[i] + ints[j] + ints[k] == 2020) {
+        for (int i = 0; i < ints.length - 1; i++)
+            for (int j = 1; j < ints.length; j++)
+                for (int k = 2; k < ints.length; k++)
+                    if (ints[i] + ints[j] + ints[k] == 2020)
                         multiplying = ints[i] * ints[j] * ints[k];
-                    }
-                }
-            }
-        }
         return multiplying;
     }
 
-    private static int part1 (List<String> input) {
-        int [] ints = new int[input.size()];
+    private static int part1(List<String> input) {
+        int[] ints = new int[input.size()];
         Arrays.setAll(ints, i -> Integer.parseInt(input.get(i)));
         int multiplying = 0;
-        for (int i = 0; i < ints.length - 1; i++) {
-            for (int j = 1; j < ints.length; j++) {
-                    if (ints[i] + ints[j] == 2020) {
-                        multiplying = ints[i] * ints[j];
-                    }
-                }
-        }
+        for (int i = 0; i < ints.length - 1; i++)
+            for (int j = 1; j < ints.length; j++)
+                if (ints[i] + ints[j] == 2020)
+                    multiplying = ints[i] * ints[j];
         return multiplying;
     }
-
 }
