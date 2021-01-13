a9b0c7c1a7f4d9554f7195f4caa52ee7a5fd5a82
Edvinas <edvinasalimas98@gmail.com>
2021-01-13 19:27:23 +0200

    Calculator for price after taxes

---
+++ src/main/java/edu/CalculatorForPriceAfterTaxes.java
@@ -0,0 +1,14 @@
+package edu;
+
+import java.util.List;
+
+public class CalculatorForPriceAfterTaxes {
+
+    private List<CountriesWithVatNumber> calculateVat;
+
+    public CalculatorForPriceAfterTaxes(List<CountriesWithVatNumber> calculateVat) {
+        this.calculateVat = calculateVat;
+    }
+
+
+}
